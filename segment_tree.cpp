bc81f84697b7b6f188500aa358e1f23b6cee595a
sfox <structfox@gmail.com>
2020-05-03 21:16:17 +0900

    세그먼트 트리 ref

---
+++ segment_tree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+// a: 배열 a
+// tree: 세그먼트 트리
+// node: 세그먼트 트리 노드 번호
+// node가 담당하는 합의 범위가 start ~ end
+ll init(vector<ll> &a, vector<ll> &tree, int node, int start, int end) {
+    if (start == end) {
+        return tree[node] = a[start];
+    } else {
+        return tree[node] = init(a, tree, node*2, start, (start+end)/2) + init(a, tree, node*2+1, (start+end)/2+1, end);
+    }
+}
+
+// node가 담당하는 구간이 start~end이고, 구해야하는 합의 범위는 left~right
+ll sum(vector<ll> &tree, int node, int start, int end, int left, int right) {
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    return sum(tree, node*2, start, (start+end)/2, left, right) + sum(tree, node*2+1, (start+end)/2+1, end, left, right);
+}
+
+void update(vector<ll> &tree, int node, int start, int end, int index, ll diff) {
+    if (index < start || index > end) return;
+    tree[node] = tree[node] + diff;
+    if (start != end) {
+        update(tree,node*2, start, (start+end)/2, index, diff);
+        update(tree,node*2+1, (start+end)/2+1, end, index, diff);
+    }
+}
